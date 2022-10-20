b5bcfa10eb16c4c2ae813bd3233cecce30331c7c
BeyondJacob <owensinnovation@gmail.com>
2022-10-20 12:39:51 -0400

    Redo Entirely

--- scripts/FetchDiscordAPI.js
+++
@@ -1,8 +0,0 @@
-
-fetch('https://discord.com/api/guilds/895085154203205633/widget.json')
-  .then(function (response) {
-    // The JSON data will arrive here
-  })
-  .catch(function (err) {
-    // If an error occured, you will catch it here
-  });
--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-// Brings the main nav to all pages.
-$(document).ready(function(){ 
-  $.get("nav_index.html", function(data) {
-    $("#nav-main").html(data);
-  });
-}); 
